20e6660a109f655d201faecaa6b62cdf53b3b49f
Marcus  = Soulo <souloinc87@gmail.com>
2018-03-20 00:10:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
-    "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
